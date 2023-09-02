b041f624866b50f49a82cfe8eab4ee4df5ab70b8
atiragramk <beizmargo@gmail.com>
2023-09-02 09:13:41 +0300

    reviewed changes

--- datakit/nmp.py
+++ datakit/nmp.py
@@ -1,12 +1,15 @@
 import numpy as np
 from math import e
 
-a = np.zeros((4, 3), dtype=np.integer)
-b = np.ones((4, 3),  dtype=np.integer)
-c = np.arange(11)
+a = np.zeros((4, 3), dtype=np.int32)
+b = np.ones((4, 3),  dtype=np.int32)
+c = np.arange(12).reshape((4, 3))
 
 x_1 = np.arange(0, 101)
 x_2 = np.arange(-10, 11)
 
-fx_1 = x_1 * 2 ** 2 + 5
+fx_1 = 2 * x_1 ** 2 + 5
 fx_2 = np.power(e, -x_2)
+
+np.column_stack((x_1, fx_1))
+np.column_stack((x_2, fx_2))
